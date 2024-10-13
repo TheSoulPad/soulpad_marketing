7f42a351e34e7495a159e2e09435b7f82747b233
Carla Severe <carla.severe@gmail.com>
2024-10-13 00:24:14 -0700

    added bg image

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,12 @@
+// src/GlobalStyles.ts
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>SoulPad</h1>
+            <p>Welcome to SoulPad, release your inner universe.</p>
+        </div>
+    );
+};
+
+export default Home;
