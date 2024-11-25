e72fa208772991bf13d84afa99a7ae3699675b04
Carla Severe <carla.severe@gmail.com>
2024-11-25 14:02:23 -0800

    created template for theme styling

---
+++ src/styles/template.ts
@@ -0,0 +1,20 @@
+//follow this template for the style of components for the themes
+
+export const styles = {
+  richTextPost: {
+    header: {},
+    content: {},
+  },
+  menuSelection: {
+    header: {},
+    cotent: {},
+    icons: {},
+  },
+  button: {},
+  card: {},
+  listBox: {},
+  mediaPlayer: {},
+  poll: {},
+  progressBar: {},
+  wordSticker: {},
+};
