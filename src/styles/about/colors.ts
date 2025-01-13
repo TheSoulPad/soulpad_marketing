31f18923c4fab2be13bd411b9b275100da7c7c10
Carla Severe <carla.severe@gmail.com>
2025-01-13 15:04:13 -0800

    created folder and colors

---
+++ src/styles/about/colors.ts
@@ -0,0 +1,35 @@
+const keyColors = {
+  orange: "#ef8216",
+  beige: "#e8d8a7",
+  brown: "#7a664b",
+  darkBrown: '#3c1b24',
+  yellow: "#efc83c",
+  lightGreen: "#a8bb53",
+  redOrange: "#c53c27"
+};
+
+const { orange, beige, brown, darkBrown, yellow, lightGreen, redOrange } = keyColors;
+
+export const colors = {
+  button: orange,
+  buttonBorder: orange,
+  buttonHover: beige,
+  buttonText: darkBrown,
+  contentBg: beige,
+  contentBorder: beige,
+  headerBg00: orange,
+  headerBg01: lightGreen,
+  headerBg02: yellow,
+  headerBorder00: orange,
+  headerBorder01: lightGreen,
+  icon: orange,
+  mainBg00: beige,
+  mainBg01: lightGreen,
+  mainBg02: yellow,
+  mainBg03: orange,
+  mainBg04: darkBrown,
+  shine: beige,
+  text01: darkBrown,
+  text02: lightGreen,
+  text03: orange,
+}
---
+++ src/styles/about/comps.ts
@@ -0,0 +1,2 @@
+import { colors as c } from "./colors";
+import { spacing as s } from "../spacing";
